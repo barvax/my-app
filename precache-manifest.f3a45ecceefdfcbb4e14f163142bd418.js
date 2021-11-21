7d5266e099856de1d6dc419169c920ad5a36db98
barvax <barvax@walla.com>
2021-11-21 19:31:33 +0200

    Updates

---
+++ precache-manifest.f3a45ecceefdfcbb4e14f163142bd418.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38c95f9cc8d95c9a112af80cac35597e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "662db2ccd97775343c77",
+    "url": "/my-app/static/css/main.9865f0d2.chunk.css"
+  },
+  {
+    "revision": "1577e1edb56cf5418327",
+    "url": "/my-app/static/js/2.7fd115b6.chunk.js"
+  },
+  {
+    "revision": "b0d2420da847bbc9cc10dc274ffdeef6",
+    "url": "/my-app/static/js/2.7fd115b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "662db2ccd97775343c77",
+    "url": "/my-app/static/js/main.48f9a036.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "906569de46a2126b6fdd06e92fcbe756",
+    "url": "/my-app/static/media/blur2.906569de.png"
+  },
+  {
+    "revision": "a6c49e90df26758c594e88d3879dec85",
+    "url": "/my-app/static/media/blurbackground.a6c49e90.jpg"
+  },
+  {
+    "revision": "50aed69bf7afbe30ca8d3c8e8ea205c5",
+    "url": "/my-app/static/media/pexels-pixabay-209831.50aed69b.jpg"
+  }
+]);
